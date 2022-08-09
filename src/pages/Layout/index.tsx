98be082264ac9bb1999caf053feda92db8ee44f7
丁翔 <1282394741@qq.com>
2022-08-09 17:34:27 +0800

    地址

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -140,7 +140,7 @@ export default function Layout() {
                         className="navList"
                         onClick={() =>
                             window.open(
-                                "http://120.26.142.251:8888/doc.html#/home"
+                                "http://42.192.225.136:8888/doc.html#/home"
                             )
                         }
                     >
